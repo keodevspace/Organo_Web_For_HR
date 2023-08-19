8e754ad3edc392491b2e68f6d008acbb6ff854e5
KEO <keoxcoelho@gmail.com~>
2023-08-19 12:18:06 -0300

    created repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Banner/>
+      <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Você é meu pãozinho de queijo
         </p>
         <a
           className="App-link"
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css';
+
+// JSX
+
+function Banner() {
+    return (
+        <header className="banner">
+        <img src="/img/Banner.png" alt="Banner principal da página"/>        
+        </header>
+    )
+    }
+
+
+export default Banner;
